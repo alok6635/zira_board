becdfb13f68e2f676694cd5908bc05d40bf16f6c
Alok Kumar <alok.kumar@dwellsmart.co.in>
2025-11-04 18:03:21 +0530

    v75

--- src/components/tag/Tag.jsx
+++ src/components/tag/Tag.jsx
@@ -1,9 +1,22 @@
 import "./tag.css";
 
-const Tag =(props)=> {
-    const {tagName,selectedTag} = props;
+const Tag = (props) => {
+    const { tagName, selectedTag, selected } = props;
+
+    const tagStyle = {
+        DEV: { backgroundColor: "#F54927" },
+        QA: { backgroundColor: "#27F579" },
+        PO: { backgroundColor: "#F527EE" },
+    }
+   const tagKeyMap={
+    DEV :"DEV",
+    QA:"QA",
+    "Product Owner":"PO"
+   }
+   const key = tagKeyMap[tagName]
+
     return (
-        <button type="button" className='tag' onClick={()=>selectedTag(tagName)} >{tagName}</button>
+        <button type="button" className='tag' onClick={() => selectedTag(tagName)} style={selected ? tagStyle[key] : {}}>{tagName}</button>
     )
 }
 export default Tag;
--- src/components/taskform/TaskForm.jsx
+++ src/components/taskform/TaskForm.jsx
@@ -3,11 +3,11 @@ import Tag from "../tag/Tag";
 import "./taskform.css";
 
 const TaskForm = () => {
-    const [taskData, setTaskData] = useState([{
+    const [taskData, setTaskData] = useState({
         task: '',
         status: 'Ready for Development',
         tags: []
-    }]);
+    });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -18,34 +18,26 @@ const TaskForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // console.log(taskData)
+    }
+
+    const checkTag=(tag)=>{
+        return taskData.tags.some((item)=> item === tag)
     }
 
     const selectedTag = (tag) => {
-        console.log("tag data heee...",taskData.tags)
-        if(taskData.tags.some((item)=> item === tag)){
-               const filterTags = taskData.filter((item)=> item !== tag);
-               setTaskData(prev=>{
-                return {...prev, tag : filterTags}
-               })
+        console.log("tag data heee...", taskData.tags)
+        if (taskData.tags.some((item) => item === tag)) {
+            const filterTags = taskData.tags.filter((item) => item !== tag);
+            setTaskData(prev => {
+                return { ...prev, tags: filterTags }
+            })
         }
-        else{
-setTaskData(prev=>{
-    return {...prev, tags:[...prev.tag, tag]}
-})
+        else {
+            setTaskData(prev => {
+                return { ...prev, tags: [...prev.tags, tag] }
+            })
         }
-        // if (taskData.tags.some((item) => item === tag)) {
-        //     const filterTags = taskData.tags.filter((item) => item !== tag);
-        //     settaskData(prev => {
-        //         return { ...prev, tags: filterTags }
-        //     })
-        // }
-        // else {
-        //     settaskData(prev => {
-        //         return { ...prev, tags: [...prev.tag, tag] }
-        //     })
-        // }
-        // console.log(tag)
+        console.log(tag)
     }
 
     return (
@@ -53,9 +45,9 @@ setTaskData(prev=>{
             <form onSubmit={handleSubmit}>
                 <input type='text' name="task" className='task_input' placeholder='Enter task-details' onChange={handleChange} />
                 <div className='task_form_bottom'>
-                    <Tag tagName="DEV" selectedTag={selectedTag} />
-                    <Tag tagName="QA" selectedTag={selectedTag} />
-                    <Tag tagName="Product Owner" selectedTag={selectedTag} />
+                    <Tag tagName="DEV" selectedTag={selectedTag} selected ={checkTag("DEV")}/>
+                    <Tag tagName="QA" selectedTag={selectedTag} selected ={checkTag("QA")}/>
+                    <Tag tagName="Product Owner" selectedTag={selectedTag} selected ={checkTag("Product Owner")}/>
                     <select className="task_status" name="status" onChange={handleChange}>
                         <option value="Ready for Development">Ready for Development</option>
                         <option value="In Progress">In Progress</option>
